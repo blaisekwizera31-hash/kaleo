bf16d0a27d6350add5399121e5092485b92f483a
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-03-12 21:50:19 +0200

    created navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,15 +1,35 @@
-import { HStack } from "@chakra-ui/react"
-import "./Navbar.css"
+import { Button, Heading, HStack } from "@chakra-ui/react";
+import "./Navbar.css";
+import {Star} from "lucide-react" 
 const Navbar = () => {
-  const navigation = ["Ranch life", "About us", "Events", "Directions", "Houses"]
+  const navigation = [
+    "Ranch life",
+    "About us",
+    "Events",
+    "Directions",
+    "Houses",
+  ];
 
   return (
-    <HStack className="nav-container" gap={24}>
+    <HStack className="nav-container" gap={21} ps={50}>
+      <Heading as="h5" pe="2">
+        KL
+      </Heading>
+      <Heading as="h5" pe="2">
+        |
+      </Heading>
       {navigation.map((item, index) => {
-        return <a key={index} className="nav-link">{item}</a>
+        return (
+          <a key={index} className="nav-link" >
+            {item}
+          </a>
+        );
       })}
+      <Button bg="black" color="white" border="2px solid red" ms="auto" me="10" mt="10">Book a visit 
+         <Star size={12} fill="white"></Star>
+      </Button>
     </HStack>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
